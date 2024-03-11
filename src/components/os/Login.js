635a14599328aeb86219d19ad86d0c44471caa24
henry-minh <95643389+henry-minh@users.noreply.github.com>
2024-03-11 08:21:00 -0400

    login page bug fixes

--- src/components/os/Login.js
+++ src/components/os/Login.js
@@ -1,32 +1,11 @@
-import { useState } from "react";
-import Draggable from "react-draggable";
 import "../../styles/Login.css";
 
 function Login({ signInFunction }) {
-  const [x, setX] = useState(window.innerWidth / 2 - 180);
-  const [y, setY] = useState(window.innerHeight / 3);
-  const handleStop = (event, dragElement) => {
-    if (dragElement.x < 0) {
-      setX(0);
-    } else if (dragElement.x > window.innerWidth - 100) {
-      setX(0);
-    } else {
-      setX(dragElement.x);
-    }
-
-    if (dragElement.y < 0) {
-      setY(0);
-    } else if (dragElement.y > window.innerHeight - 88) {
-      setY(0);
-    } else {
-      setY(dragElement.y);
-    }
-  };
   return (
     <div id="login-container">
-      <Draggable handle=".browser-header-container" defaultPosition={{ x: x, y: y }} onStop={handleStop} position={{ x: x, y: y }}>
-        <div id="draggable-content" style={{ width: "400px", height: "125px" }}>
-          <div id="browser-header-container" className="browser-header-container">
+      <div id="login-browser">
+        <div id="draggable-content" style={{ width: "55vw", height: "14vh", maxWidth: "400px", maxHeight: "125px", position: "relative" }}>
+          <div id="browser-header-container" className="browser-header-container" style={{ cursor: "auto" }}>
             <p id="browser-header-name">Windows 95</p>
           </div>
           <div id="login-content">
@@ -47,7 +26,7 @@ function Login({ signInFunction }) {
             </div>
           </div>
         </div>
-      </Draggable>
+      </div>
     </div>
   );
 }
